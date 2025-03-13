869ec23c5d63cca6c8d27c8f3d847311e1d24344
parth <parthg@andrew.cmu.edu>
2025-03-13 00:49:49 -0400

    [launch] Add realsense launching in launch file

--- launch/snaak_vision_launch.py
+++ launch/snaak_vision_launch.py
@@ -6,5 +6,11 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='snaak_vision',
             executable='vision_node.py',
-            name='snaak_vision')
+            name='snaak_vision'),
+
+        launch_ros.actions.Node(
+            package="realsense2_camera",
+            executable="realsense2_camera_node",
+            name="realsense2_camera_node"
+        )
     ])
