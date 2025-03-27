61a902c6938cd70820986a543f01c75645bf14ea
oberton31 <bertonoliver50@gmail.com>
2025-03-27 17:33:38 -0400

    fixed vision launch file

--- launch/snaak_vision_launch.py
+++ launch/snaak_vision_launch.py
@@ -6,7 +6,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # Include the RealSense launch file
     realsense_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             os.path.join(
